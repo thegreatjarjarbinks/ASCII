b110895b255bb99eb426d89da0269374f12e69a0
RyanStLouis <ryan.stlouis@ocsbstudent.ca>
2015-10-16 13:03:58 -0400

    ASCII

---
+++ ASCII/ASCII.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ASCII
+{
+    partial class frmASCII
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(107, 12);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.Location = new System.Drawing.Point(73, 70);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(152, 160);
+            this.lstNumbers.TabIndex = 1;
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Location = new System.Drawing.Point(13, 42);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(281, 13);
+            this.lblSearch.TabIndex = 2;
+            this.lblSearch.Text = "Press the button to see all of the ASCII numbers from A - Z";
+            // 
+            // frmASCII
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 279);
+            this.Controls.Add(this.lblSearch);
+            this.Controls.Add(this.lstNumbers);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "frmASCII";
+            this.Text = "ASCII";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ListBox lstNumbers;
+        private System.Windows.Forms.Label lblSearch;
+    }
+}
+
---
+++ ASCII/ASCII.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Created by: Alex Mathias
+ * Created on: 16-Oct-2015
+ * Created for: ICS3U
+ * Daily Assignment – Unit 3-11
+ * This program displays ASCII numbers from A-Z
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ASCII
+{
+    public partial class frmASCII : Form
+    {
+        public frmASCII()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            //Variables
+            const int maxNumber = 91;
+            //Process
+            for (int loopCounter = 65; loopCounter != maxNumber; loopCounter++)
+            {
+                this.lstNumbers.Items.Add(Char.ConvertFromUtf32(loopCounter)+ "----->" + loopCounter);
+            }
+        }
+    }
+}
